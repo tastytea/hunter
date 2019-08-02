5fea1db0c57dffad6666a1774681b8263531f428
tastytea <tastytea@tastytea.de>
2019-08-02 17:44:50 +0200

    Test....

--- cmake/projects/libconfig/hunter.cmake
+++ cmake/projects/libconfig/hunter.cmake
@@ -4,10 +4,9 @@
 # !!! DO NOT PLACE HEADER GUARDS HERE !!!
 
 include(hunter_add_version)
-include(hunter_configuration_types)
-include(hunter_pick_scheme)
-include(hunter_download)
 include(hunter_cacheable)
+include(hunter_download)
+include(hunter_pick_scheme)
 include(hunter_cmake_args)
 
 hunter_add_version(
@@ -16,25 +15,18 @@ hunter_add_version(
     VERSION
     1.7.2-a0301bd
     URL
-    "https://github.com/hyperrealm/libconfig/archive/a0301bde126c2b08b50e789fd025a8c513c914c3.tar.gz"
+    "https://github.com/tastytea/libconfig/archive/cmake_config_target.tar.gz"
     SHA1
-    a9f899e8ea6d2d55969b6fc619af116a1ce53e0e
-    )
-
-hunter_configuration_types(libconfig CONFIGURATION_TYPES Release)
-hunter_pick_scheme(DEFAULT url_sha1_autotools)
-hunter_cacheable(libconfig)
+    6eab3c39f39214dd92cbb9464da813ccb5d9db64
+)
 
 hunter_cmake_args(
     libconfig
     CMAKE_ARGS
-        BOOTSTRAP=autoreconf
-        PKGCONFIG_EXPORT_TARGETS=libconfig++;libconfig
-        EXTRA_FLAGS=--enable-shared=no --disable-examples --disable-doc
+        BUILD_EXAMPLES=OFF
+        BUILD_TESTS=OFF
 )
 
-hunter_download(PACKAGE_NAME libconfig
-    PACKAGE_INTERNAL_DEPS_ID "1"  # Increment for each new pull request
-    PACKAGE_UNRELOCATABLE_TEXT_FILES
-    lib/pkgconfig/libconfig++.pc
-    lib/pkgconfig/libconfig.pc)
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(libconfig)
+hunter_download(PACKAGE_NAME libconfig)
